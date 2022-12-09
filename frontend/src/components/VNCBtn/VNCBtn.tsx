e0b9612a0a0576fa8a4fc73b90a82737db64fa2b
Скоробогатый Евгений <evskorobogatij@gmail.com>
2022-12-09 16:24:16 +0300

    Изменения кнопки VNC

--- frontend/src/components/VNCBtn/VNCBtn.tsx
+++ frontend/src/components/VNCBtn/VNCBtn.tsx
@@ -25,29 +25,15 @@ export const VNCBtn = ({ ip, hovered }: VNCBtnProps): JSX.Element => {
 
   return (
     <>
-      {ipList.length === 1 ? (
-        <button
-          className={clsx(
-            'rounded-lg border border-blue-800 bg-white px-3 py-2 text-xs font-medium text-blue-800 transition duration-300 ease-in-out hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800',
-            hovered ? 'opacity-100' : 'opacity-0'
-          )}
-          onClick={onClick}
-        >
-          VNC
-        </button>
-      ) : (
-        <>
-          <button
-            onClick={handleShowMenu}
-            className={clsx(
-              'rounded-lg border border-blue-800 bg-white px-3 py-2 text-xs font-medium text-blue-800 transition duration-300 ease-in-out hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800',
-              hovered ? 'opacity-100' : 'opacity-0'
-            )}
-          >
-            VNC
-          </button>
-        </>
-      )}
+      <button
+        onClick={ipList.length === 1 ? onClick : handleShowMenu}
+        className={clsx(
+          'rounded-lg border border-blue-800 bg-white px-3 py-2 text-xs font-medium text-blue-800 transition duration-300 ease-in-out hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800',
+          hovered ? 'opacity-100' : 'opacity-0'
+        )}
+      >
+        VNC
+      </button>
     </>
   )
 }
